afe1007c167e151a84ba08c79b914b0fe24b939b
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-13 19:50:08 -0300

    Update repo

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/html" });
   fs.createReadStream("./public/index.html").pipe(res);
+
+  res.writeHead(200, { "content-type": "text/css" });
+  fs.createReadStream("./public/styles.css").pipe(res);
 });
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-//testando o watch do node
 const index = "./public/index.html";
 let fsWait = false;
 
-io.on("connection", (socket) => {
-  // console.log(updatePage);
+io.on("connect", (socket) => {
   fs.watch(index, (event, filename) => {
     if (fsWait) return;
     fsWait = setTimeout(() => {
@@ -20,7 +21,21 @@ io.on("connection", (socket) => {
     }, 100);
     console.log(`${filename} file Changed`);
 
-    socket.emit("conexao", true);
+    socket.emit("connection", true);
+  });
+});
+
+const indexcss = "./public/styles.css";
+let fsWaitCss = false;
+io.on("connect", (socket) => {
+  fs.watch(indexcss, (event, filename) => {
+    if (fsWaitCss) return;
+    fsWaitCss = setTimeout(() => {
+      fsWaitCss = false;
+    }, 100);
+    console.log(`${filename} file Changed`);
+
+    socket.emit("connection", true);
   });
 });
 
